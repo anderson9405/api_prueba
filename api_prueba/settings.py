6e2d0df86701754c79d4b6aea0d7417ca36dd070
Anderson <anderson052594@gmail.com>
2024-03-23 08:35:19 -0500

    Database changes

--- api_prueba/settings.py
+++ api_prueba/settings.py
@@ -83,9 +83,17 @@ WSGI_APPLICATION = 'api_prueba.wsgi.application'
 
 import dj_database_url
 
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default='sqlite:///db.sqlite3',
+#         conn_max_age=600
+#     )
+# }
+
 DATABASES = {
     'default': dj_database_url.config(
-        default='sqlite:///db.sqlite3',
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/mysite',
         conn_max_age=600
     )
 }
